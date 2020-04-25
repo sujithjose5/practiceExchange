105e08c6d2c2431fd256483ace850aa3c8e916d5
Mohammed Shabaj Ahmed <shabaj_ahmed@hotmail.co.uk>
2020-04-25 19:45:31 +0100

    Added home page code.

--- practice_exchange/lib/main.dart
+++ practice_exchange/lib/main.dart
@@ -1,8 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -21,10 +20,6 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -50,68 +45,276 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Icon _searchIcon = new Icon(Icons.search);
 
-  void _incrementCounter() {
+  Widget _appBarTitle = RaisedButton(
+      color: Colors.transparent,
+      onPressed: () {},
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+          side: BorderSide(color: Colors.white)),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'Languages',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              color: Colors.white,
+            ),
+          ),
+          Icon(
+            Icons.arrow_drop_down,
+            color: Colors.white,
+          ),
+        ],
+      ));
+
+  final TextEditingController _filter = TextEditingController();
+
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+  TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Saved',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: New Article',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 4: Notification',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 5: Profile',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
+  }
+
+  Widget categoryList(String title) {
+    return Container(
+      width: 160.0,
+      child: Card(
+        color: Colors.blue,
+        child: Column(
+          children: <Widget>[
+            Center(child: Text(title)),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget getCategories() {
+    //TODO: request data from database
+    //TODO: build a list of categories with result of database
+
+    //tests data for now
+    List<String> categoryTitles = [
+      'Disaster prep',
+      'healthcare best practices',
+      'Infection control',
+    ];
+
+    return ListView.builder(
+      scrollDirection: Axis.horizontal,
+      shrinkWrap: true,
+      itemCount: categoryTitles.length,
+      itemBuilder: (BuildContext context, int index) {
+        return categoryList(categoryTitles[index]);
+      },
+    );
+    categoryList('Disaster prep');
+  }
+
+  Widget articleList(String title, int ranking) {
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: Container(
+        width: 160.0,
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: Colors.grey,
+              width: 1,
+            ),
+            borderRadius: BorderRadius.circular(4),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      //User profile icon
+                      Text(
+                        'firstname lastname',
+                      ),
+                      Text(' | organisation'),
+                    ],
+                  ),
+                  Text(title),
+                  Text('Some text.'),
+                ],
+              ),
+              Column(
+                children: <Widget>[
+                  Icon(
+                    Icons.arrow_drop_up,
+                    color: Colors.black,
+                  ),
+                  Text('$ranking'),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget getArticles() {
+    List<String> categoryTitles = [
+      'Article title',
+      'Article title 2',
+      'Article title 3',
+      'Article title 4',
+      'Article title 5',
+      'Article title 6',
+      'Article title 7',
+      'Article title 8',
+    ];
+
+    return ListView.builder(
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
+      itemCount: categoryTitles.length,
+      itemBuilder: (BuildContext context, int index) {
+        return articleList(categoryTitles[index], 10);
+      },
+    );
+  }
+
+  void _searchPressed() {
+    setState(() {
+      if (this._searchIcon.icon == Icons.search) {
+        this._searchIcon = new Icon(Icons.close);
+        this._appBarTitle = new TextField(
+          controller: _filter,
+          decoration: new InputDecoration(
+              prefixIcon: new Icon(Icons.search), hintText: 'Search...'),
+        );
+      } else {
+        this._searchIcon = new Icon(Icons.search);
+        this._appBarTitle = new Text('Search Example');
+        _filter.clear();
+      }
+    });
+  }
+
+  Widget _bottomNavigationBar() {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Icon(Icons.home),
+          title: Text('Home'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bookmark),
+          title: Text('Saved'),
+        ),
+        BottomNavigationBarItem(
+          //change new article for pen
+          icon: Icon(Icons.pages),
+          title: Text('New Article'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.notifications),
+          title: Text('Notification'),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          title: Text('Profile'),
+        ),
+      ],
+      currentIndex: _selectedIndex,
+      type: BottomNavigationBarType.fixed,
+      unselectedItemColor: const Color(0xff797979),
+      selectedItemColor: Colors.blue,
+      onTap: _onItemTapped,
+    );
+  }
+
+  Widget _divider() {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.only(left: 20.0, right: 10.0),
+        child: Divider(
+          color: Colors.black,
+          height: 5,
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+        title: _appBarTitle,
+        leading: new IconButton(
+          icon: _searchIcon,
+          onPressed: _searchPressed,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Text('Topics for you'),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 20.0),
+            height: 80.0,
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                getCategories(),
+              ],
+            ),
+          ),
+          // add divider
+          _divider(),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Container(
+              child: Text('Your Daily Read'),
+            ),
+          ),
+          getArticles(),
+        ],
+      ),
+//      Center(
+//        child: _widgetOptions.elementAt(_selectedIndex),
+//      ),
+      bottomNavigationBar: _bottomNavigationBar(),
     );
   }
 }
